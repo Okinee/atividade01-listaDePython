958bda6e176da3a60103834b7c8e9b89681c736f
KauaSCS <139986803+Okinee@users.noreply.github.com>
2026-03-19 08:38:57 -0300

    commit da atividade 7
    Foi enviado o commit da atividade 7

---
+++ index7.py
@@ -0,0 +1,8 @@
+# Crie um progrma que peça a base e a altura de um triangulo, calcule sua area e mostre o resultado na tela.
+
+base = int(input("Digite o valor da base"))
+altura = int(input("Digite o valor da altura"))
+
+area = base*altura
+
+print("\nDe acordo com a area: {}, e a base {}, o valor da area foi de: {}".format(base, altura, area))
